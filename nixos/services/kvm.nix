85c7ba75b463b8fce129f78b0c82ed85925d763c
RikkaNekoo <rikka@rikka.im>
2026-08-11 15:10:25 +0800

    [F] homelab.service.libvirtd: shutdown VMs when host shutdown

--- nixos/services/kvm.nix
+++ nixos/services/kvm.nix
@@ -1,6 +1,18 @@
 _: {
   virtualisation.libvirtd.enable = true;
 
+  systemd.services.libvirt-guests = {
+    enable = true;
+
+    serviceConfig = {
+      Environment = [
+        "ON_SHUTDOWN=shutdown"
+        "ON_BOOT=ignore"
+        "SHUTDOWN_TIMEOUT=300"
+      ];
+    };
+  };
+
   users.users.rikka.extraGroups = [
     "libvirtd"
     "kvm"
