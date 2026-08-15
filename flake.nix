336fac4c003d2c610298b66dd0ff0d8fb0a6a32d
RikkaNekoo <rikka@rikka.im>
2026-08-15 14:33:59 +0800

    [U] flake: vscode-server no more follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -11,10 +11,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
-    vscode-server = {
-      url = "github:nix-community/nixos-vscode-server";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
