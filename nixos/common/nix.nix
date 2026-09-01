31ce1bd307e2dbe2f4b6e2dc9289a5b09d36d09d
RikkaNekoo <rikka@rikka.im>
2026-09-01 19:27:57 +0800

    [U] nixos.nix: use common configurations

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -1,38 +1,14 @@
-{ pkgs, inputs, lib, ... }:
-
-{
+_: {
+  imports = [
+    ../../common/nix.nix
+  ];
   nix = {
-    settings = {
-      experimental-features = [ "nix-command" "flakes" ];
-      auto-optimise-store = true;
-      substituters = [ "https://mirror.sjtu.edu.cn/nix-channels/store" ];
-      # Disable the built-in flake registry to speed up evaluation
-      flake-registry = "";
-      trusted-users = [
-        "@wheel"
-        "root"
-      ];
-    };
-
     gc = {
       automatic = true;
       dates = "weekly";
       options = "--delete-older-than 7d";
     };
-
     optimise.automatic = true;
-
-    # This is important. It locks nixpkgs registry used in nix shell
-    # to the same of flakes. Saves time.
-    registry =
-        {
-          pkgs.flake = inputs.self;
-        }
-        // lib.mapAttrs (_: flakes: {flake = flakes;}) inputs;
-
-      channel.enable = false; # remove nix-channel related tools & configs, we use flakes instead.
-
-      nixPath = ["nixpkgs=${inputs.nixpkgs}"];
   };
 
   hardware.enableRedistributableFirmware = true;
