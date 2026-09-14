0148a373673b6330b83016e85fd257e1c09aa180
RikkaNekoo <rikka@rikka.im>
2026-09-14 22:25:46 +0800

    [+] service.podman: add unqualified-search-registries
    this fix `Error: short-name "debian" did not resolve to an alias and no unqualified-search registries are defined in "/etc/containers/registries.conf"`

--- nixos/services/podman.nix
+++ nixos/services/podman.nix
@@ -1,9 +1,21 @@
 _: {
-  virtualisation.podman = {
-    enable = true;
-    autoPrune.enable = true;
-    dockerCompat = true;
-    dockerSocket.enable = true;
+  virtualisation = {
+    oci-containers.backend = "podman";
+    podman = {
+      enable = true;
+      autoPrune.enable = true;
+      dockerCompat = true;
+      dockerSocket.enable = true;
+    };
+    containers = {
+      registries.settings = {
+        unqualified-search-registries = [ "docker.io" ];
+
+        registry = [
+          { location = "docker.io"; }
+          { location = "quay.io"; }
+        ];
+      };
+    };
   };
-  virtualisation.oci-containers.backend = "podman";
 }
